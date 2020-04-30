6d410dc7e807ea74949a7b3408d7db81dee0ffed
Ubuntu <ubuntu@ip-172-31-92-189.ec2.internal>
2020-04-30 18:05:59 +0000

    I believe I'm finished. A new database was created after deleting the old one.

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,16 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
+
+user = 'shaz'
+password = 'shaz123'
+database = 'info3180-lab5'
+default = "postgresql://%s:%s@localhost/%s" % (user,password,database)
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:comp3180@localhost/lab5"
+app.config['SECRET_KEY'] = "xqJH87skjvakhvauiweuw73ejwyOIJGhcauGSALSAOIHB.au8swolJKJZDSUSDNSDCHUjdsjdiuenhbs7u329236123t532912-9jas;kkujgs"
+app.config['SQLALCHEMY_DATABASE_URI'] = default
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
